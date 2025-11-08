a5b115ea334272033d4f3a1852f61469132c00c7
ved-emergeflow <ved@emergeflow.com>
2025-11-08 21:21:03 +0530

    FastAPI app file

--- src/Summary-Critic/main.py
+++ src/Summary-Critic/main.py
@@ -1,15 +1,35 @@
 from fastapi import FastAPI
+from langchain_core.messages import HumanMessage
 from summarizer_critic_agent import load_agent
 
+agent = load_agent()
 app = FastAPI()
+
+@app.get("/")
+def run():
+    return{
+        'message': 'Hi'
+    }
 
 @app.get('/status')
 def status():
     return{
-        'message': 'Status: Running'
+        'status': 'Running'
     }
 
-@app.get('/summarizer')
-def get_agent():
+@app.post('/summarizer/chat')
+def get_agent(input: str):
 
-    agent = load_agent()
+    res = agent.invoke(
+        {
+            'messages': HumanMessage(content=input),
+            'input': input,
+            'counter': 0,
+            'threshold': 9
+        }
+    )
+
+    return {
+        'message': res['summary']
+    }
+
